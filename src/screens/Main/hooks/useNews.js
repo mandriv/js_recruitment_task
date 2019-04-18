f338d5f85966f506c5f736ad7fad9a92330df279
mandriv <wojciech.cichoradzki@gmail.com>
2019-04-17 21:00:36 -0700

    Fix error handling and simplify params extraction

--- src/screens/Main/hooks/useNews.js
+++ src/screens/Main/hooks/useNews.js
@@ -24,23 +24,19 @@ export default function useAdmins() {
       fromDate.setMonth(fromDate.getMonth() - 1);
       // specify params based on control
       const params = {
+        q: searchQuery,
+        section: activeSection === 'all' ? '' : activeSection,
         page: activePage,
         'from-date': fromDate.toISOString(),
       };
-      if (searchQuery !== '') {
-        params.q = searchQuery;
-      }
-      if (activeSection !== 'all') {
-        params.section = activeSection;
-      }
       // make request
-      const response = await News.search(params);
+      const { response } = await News.search(params);
       const newsItems = response.results.map(r => transformToNewsItem(r));
       setNews(newsItems);
       setError(false);
     } catch (err) {
-      console.warn(err); // eslint-disable-line
-      setError(err.message);
+      console.warn(JSON.stringify(err)); // eslint-disable-line
+      setError('Oops! Something went wrong.');
     } finally {
       setLoading(false);
     }
--- src/utils/Api/Api.js
+++ src/utils/Api/Api.js
@@ -7,27 +7,38 @@ const defaultParams = {
 };
 const defaultConfig = {};
 
+// acts as a axios-like wrapper around fetch
 const request = async (endpoint = '', params = defaultParams, config = defaultConfig) => {
   // construct url-encoded params string
   const paramsString = Object.entries({ ...defaultParams, ...params }).reduce((str, entry) => {
     const key = entry[0];
     const value = entry[1];
+    if (!key || !value) return str;
     const keyValue = `${key}=${value}`;
     return str === '' ? `?${keyValue}` : `${str}&${keyValue}`;
   }, '');
 
   const url = `${CORS}${BASE_URL}${endpoint}${paramsString}`;
   const mergedConfig = { ...defaultConfig, ...config };
-  try {
-    const fetchResponse = await fetch(url, mergedConfig);
+  const fetchResponse = await fetch(url, mergedConfig);
+  // detect error
+  if (!fetchResponse.ok) {
+    let errorResponseBody = null;
     if (fetchResponse.headers.get('Content-Type') === 'application/json') {
-      const { response } = await fetchResponse.json();
-      return response;
+      errorResponseBody = await fetchResponse.json();
+    } else {
+      errorResponseBody = await fetchResponse.text();
     }
-    return await fetchResponse.text();
-  } catch (err) {
-    throw new Error(err);
+    const error = new Error(fetchResponse.statusText);
+    error.statusCode = fetchResponse.status;
+    error.data = errorResponseBody;
+    throw error;
   }
+  // response ok, transform response body
+  if (fetchResponse.headers.get('Content-Type') === 'application/json') {
+    return fetchResponse.json();
+  }
+  return fetchResponse.text();
 };
 
 const Api = {
