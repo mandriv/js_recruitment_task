5be1a65ef5b63ca831d34bcc9837a76330b206e4
mandriv <wojciech.cichoradzki@gmail.com>
2019-04-17 18:12:24 -0700

    Make htmlFor with matching id enough for input labels

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,13 @@ module.exports = {
   },
   "rules": {
     "import/no-absolute-path": 0,
+    "jsx-a11y/label-has-for": [ 2, {
+      "required": {
+        "every": [ "id" ]
+      },
+    }],
+    "jsx-a11y/label-has-associated-control": [ 2, {
+      "assert": "htmlFor",
+    }],
   }
 }
